febfe20685d17085d6d0bfed2372692c8fd7030a
testkkj <46573845+testkkj@users.noreply.github.com>
2023-02-13 01:36:54 +0900

    스프링 웹 최소 세팅
    웹 디펜던시 추가
    접속 관련 어노테이션 추가

--- springbootmaven/src/main/java/com/example/springbootmaven/SpringbootmavenApplication.java
+++ springbootmaven/src/main/java/com/example/springbootmaven/SpringbootmavenApplication.java
@@ -2,10 +2,20 @@ package com.example.springbootmaven;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+// restcontroller 추가
+@RestController
 @SpringBootApplication
 public class SpringbootmavenApplication {
 
+	// requestmapping 추가
+	@RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootmavenApplication.class, args);
 	}
