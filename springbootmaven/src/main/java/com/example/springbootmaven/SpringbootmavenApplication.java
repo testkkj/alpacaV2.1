8280fcca19c0e377d099d4ac35136081e951cb26
testkkj <46573845+testkkj@users.noreply.github.com>
2023-02-13 01:31:47 +0900

    스프링 빈 프로젝트 생성
    spring initialize 기본생성

---
+++ springbootmaven/src/main/java/com/example/springbootmaven/SpringbootmavenApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootmaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootmavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootmavenApplication.class, args);
+	}
+
+}
