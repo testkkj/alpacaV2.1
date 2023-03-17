4324f4629c3082286b64f1297c88bab849f2f9d9
testkkj <46573845+testkkj@users.noreply.github.com>
2023-03-18 06:36:44 +0900

    Initial commit

---
+++ src/test/java/com/example/alpacaV21/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.alpacaV21;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
