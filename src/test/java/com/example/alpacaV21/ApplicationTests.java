072ffdfee60c81893787e6fbcca27b0aa3a492c1
testkkj <46573845+testkkj@users.noreply.github.com>
2023-05-19 00:23:38 +0900

    DB 연결 테스트
    postgresql DB연결 테스트코드

--- src/test/java/com/example/alpacaV21/ApplicationTests.java
+++ src/test/java/com/example/alpacaV21/ApplicationTests.java
@@ -1,5 +1,8 @@
 package com.example.alpacaV21;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +11,24 @@ class ApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	private static final String DRIVER = "org.postgresql.Driver";
+	private static final String URL = "jdbc:postgresql://localhost:5432/postgres"; // jdbc:mysql://127.0.0.1:3306/여러분이 만드신 스키마이름
+	private static final String USER = "postgres"; //DB 사용자명
+	private static final String PW = "postgres";   //DB 사용자 비밀번호
+	
+	@Test
+	public void testConnection() throws Exception{
+	 Class.forName(DRIVER);
+	 
+	 try(Connection con = DriverManager.getConnection(URL, USER, PW)){
+	  System.out.println("성공");
+	  System.out.println(con);
+	 }catch (Exception e) {
+	  System.out.println("에러발생");
+	  e.printStackTrace();
+	 }
 	}
 
 }
